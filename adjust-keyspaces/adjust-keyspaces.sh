68548e0fc75014c3b8cd3538749ff53be0c2d423
Alex Ott <alexey.ott@datastax.com>
2019-05-03 15:07:20 +0200

    first version of the script to adjust replication factors
    Signed-off-by: Alex Ott <alexey.ott@datastax.com>

---
+++ adjust-keyspaces/adjust-keyspaces.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+#
+# File: adjust-keyspaces.sh
+#
+# Created: Friday, May  3 2019
+#
+
+rep_factor=3
+fix_system=1
+keyspaces=("system_auth" "system_distributed"  "dse_security" "solr_admin" "dse_perf" "dse_leases" "dse_analytics" "dsefs" '"HiveMetaStore"')
+
+function usage() {
+    echo "Usage: $0 [-n replication_factor] [keyspace]"
+    echo "Defaults:"
+    echo "   replication_factor = $rep_factor"
+    echo "   keyspace = name of keyspace to fix replication factor."
+    echo "              fix for system keyspaces if not specified"
+}
+
+while getopts ":ht:n:" opt; do
+    case $opt in
+        n) rep_factor=$OPTARG
+           ;;
+        h) usage
+           exit 0
+           ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
+ksname="${1:-}"
+if [ -n "$ksname" ] ; then
+    keyspaces=($ksname)
+    fix_system=0
+fi    
+
+my_pid=$$
+
+DSETOOL_FILE=/tmp/dsetool-out-${my_pid}.txt
+SCHEMA_FILE=/tmp/cqlsh-schema-${my_pid}.txt
+TMP_FILE=/tmp/tmp-${my_pid}.txt
+
+# File with commands to execute
+CQL_FILE=/tmp/fix-keyspaces-${my_pid}.cql
+rm -f $CQL_FILE
+touch $CQL_FILE
+
+dsetool status > $DSETOOL_FILE
+if cat $DSETOOL_FILE|grep -e '^[UD][NJLM] '|grep -v -e '^UN ' > /dev/null 2>&1 ; then
+    echo "Cluster has nodes with non-UN status, can't adjust replication factor!"
+    exit 1
+fi
+
+cqlsh -e 'DESCRIBE FULL SCHEMA;' > $SCHEMA_FILE
+
+declare -A all_ks
+for i in `cat $SCHEMA_FILE|grep 'CREATE KEYSPACE'|grep -e 'SimpleStrategy\|NetworkTopologyStrategy'|sed -e 's|^CREATE KEYSPACE \([^ ]*\).*$|\1|'`; do
+    all_ks[$i]=1
+done
+#echo "All keyspaces=${!all_ks[@]}"
+
+declare -A all_dcs
+
+curr_dc=''
+cnt=0
+while read -d $'\n' line ; do
+    if [ -z "$curr_dc" ] ; then
+        curr_dc=`echo "$line"|sed -e 's|^DC: \([^ ]*\) .*$|\1|'`
+        if [ -z "$curr_dc" ]; then
+            echo "Can't identify DC!"
+            break;
+        fi
+    fi
+    if [ -z "$line" ] ; then
+        max_rf=$rep_factor
+        if [ $rep_factor -gt $cnt ]; then
+            max_rf=$cnt
+        fi
+        echo "$curr_dc has $cnt nodes max RF=$max_rf"
+        all_dcs[$curr_dc]=$max_rf
+        curr_dc=''
+        cnt=0
+    fi
+    if echo "$line"|grep -e '^[UD][NJLM] ' > /dev/null 2>&1 ; then
+        ((cnt++))
+    fi
+done < $DSETOOL_FILE
+#echo "All DCs=${!all_dcs[@]}"
+
+to_repair=()
+for i in "${keyspaces[@]}"; do
+#    echo "Processing $i"
+    if [ -z "${all_ks[$i]}" ]; then
+        if [ $fix_system = "0" ]; then 
+            echo "$i not in the list of existing keyspaces!"
+        fi
+        continue
+    fi
+    echo -n "ALTER KEYSPACE $i WITH replication = {'class': 'NetworkTopologyStrategy'" >> $CQL_FILE
+    for key in "${!all_dcs[@]}"; do
+        echo -n ", '$key': ${all_dcs[$key]}" >> $CQL_FILE
+    done
+    echo "};" >> $CQL_FILE
+    to_repair+=($i)
+done
+
+ret_code=0
+if [ ${#to_repair[@]} -eq 0 ]; then
+    rm -f $CQL_FILE
+    echo "No keyspaces processed!"
+    ret_code=1
+else
+    #cat $CQL_FILE
+    echo "Please execute command 'cqlsh -f $CQL_FILE' to adjust replication factor for keyspaces"
+    echo "After that, execute following commands on each node of the cluster:"
+    for i in "${to_repair[@]}" ; do
+        echo "nodetool repair $i"
+    done
+fi
+
+# remove already processed files
+rm -f $SCHEMA_FILE $DSETOOL_FILE
+
+exit $ret_code
